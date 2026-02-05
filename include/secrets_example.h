23c1be7e27f42312992745f09373baf078d8b1bc
Rania Atmani Garmavich <raniaatmani22@gmail.com>
2026-02-05 10:03:12 -0800

    Convert to PlatformIO structure (src/include) and add config files

---
+++ include/secrets_example.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+// Copy this file to secrets.h and fill in your Wi-Fi credentials.
+// secrets.h is ignored by git.
+
+#define WIFI_SSID "YOUR_SSID"
+#define WIFI_PASS "YOUR_PASSWORD"
